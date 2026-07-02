8fcc51f092221e7f3f4f339f18d1777cee938a05
Chalisa <chalisa.nk@mail.wu.ac.th>
2026-07-02 12:23:29 +0700

    Initial commit

---
+++ scritp.js
@@ -0,0 +1,8 @@
+window.onscroll = function () {
+    const btn = document.querySelector('.back-to-top');
+    
+    btn.style.display =
+        (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200)
+        ? 'block'
+        : 'none';
+};
